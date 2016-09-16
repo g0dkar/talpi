b24207a2ede06bdc0ef61da9caa96452fcc32a79
kelvin <kelvsousa@gmail.com>
2016-09-16 15:18:05 -0300

    Comentários JavaDoc escritos para o Modelo Estado e para a Enum EstadoEnum

--- model/src/main/java/br/com/talpi/estado/EstadoEnum.java
+++ model/src/main/java/br/com/talpi/estado/EstadoEnum.java
@@ -1,5 +1,11 @@
 package br.com.talpi.estado;
 
+/**Enumeration do tipo Estado
+ * @author Rafael Lins
+ * @version 0.1
+ * @since Beta-release
+ */
+
 public enum EstadoEnum {
 	CRIADO,
 	INICIADO,
