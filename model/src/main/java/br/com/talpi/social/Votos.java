7484995567a69d359accd56289f6921f43b1d659
kelvin <kelvsousa@gmail.com>
2016-09-16 16:01:02 -0300

    Comentários JavaDoc escritos para os modelos da categoria Social: Comentario, Comentarios e Votos

--- model/src/main/java/br/com/talpi/social/Votos.java
+++ model/src/main/java/br/com/talpi/social/Votos.java
@@ -43,26 +43,44 @@ public class Votos implements Serializable {
 			alertaAutomatico = false;
 		}
 	}
+
+	/** Método para retorno do Id
+	 *   @return long - id*/
 	
 	public Long getId() {
 		return id;
 	}
 
+	/** Método para setar o id
+	 * @param id long - Id*/
+
 	public void setId(final Long id) {
 		this.id = id;
 	}
+
+	/** Método para retorno dos votos Positivos
+	 *   @return long - Positivos*/
 
 	public long getPositivos() {
 		return positivos;
 	}
 
+	/** Método para setar o votos Positivos
+	 * @param positivo long - Positivos*/
+
 	public void setPositivos(final long positivos) {
 		this.positivos = positivos;
 	}
 
+	/** Método para retorno dos votos Negativos
+	 *   @return long - Negativos*/
+
 	public long getNegativos() {
 		return negativos;
 	}
+
+	/** Método para setar o votos Negativos
+	 * @param positivo long - Negativos*/
 
 	public void setNegativos(final long negativos) {
 		this.negativos = negativos;
