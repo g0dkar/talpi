8a056e56e7f649f9d29b766916d0632ad1ba9fd8
Victor Frazão Lelis de Aragão <vflaragao@gmail.com>
2016-09-29 06:22:55 -0300

    Restrutura configuração de projeto

---
+++ frontend/app/config/config.controller.js
@@ -0,0 +1,59 @@
+(function(angular) {
+
+	'use strict';
+
+	angular
+		.module('talpi')
+		.controller('ConfigCtrl', ConfigCtrl);
+
+	ConfigCtrl.$inject = ['projectService', '$rootScope', '$cookieStore', '$state'];
+	function ConfigCtrl(projectService, $rootScope, $cookieStore, $state) {
+
+		var vm = this;
+
+		vm.loading = false;
+		vm.project = {};
+
+		vm.cancel = cancel; 
+		vm.save = save;
+
+		activate();
+
+		/////////////////////
+
+		function activate() {
+			projectService.get($rootScope.project.id).then(function(response) {
+				console.log(response.data);
+				vm.project = response.data;
+			}, function(err) {
+				console.error(err);
+				//TODO treat request error;
+			})
+		}
+
+		function save() {
+			vm.loading = true;
+			projectService.post(vm.project).then(function(response) {
+				projectService.get($rootScope.project.id).then(function(response) {
+					$cookieStore.put('project', response.data);
+					$rootScope.project = response.data;
+					vm.project = response.data;
+					vm.loading = false;
+					$state.go('project.requirement');
+				}, function(err) {
+					console.error(err);
+					//TODO treat request error
+				});
+			}, function(err) {
+				console.log('Erro no requisição');
+				//TODO treat request error
+				vm.loading = false;
+			});
+		};
+
+		function cancel() {
+			$state.go('project.requirement');
+		}
+	};
+
+})(angular);
