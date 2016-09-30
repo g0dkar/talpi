b656d0ab88f2b1e148388eabe3d887f3821973d9
Victor Frazão Lelis de Aragão <vflaragao@gmail.com>
2016-09-30 07:56:48 -0300

    Adiciona comentarios e votos de requisitos e tarefas ao escopo global da aplicação

--- frontend/app/app.run.js
+++ frontend/app/app.run.js
@@ -11,15 +11,24 @@
 		
 		$rootScope.logout = logout;
 		$rootScope.checking = false;
-		$rootScope.project = $cookieStore.get('project') || null;
+
+		$rootScope.taskVotes = $cookieStore.get('taskVotes') || {};
+		$rootScope.taskComments = $cookieStore.get('taskComments') || {};
+		$rootScope.requirementVotes = $cookieStore.get('requirementVotes') || {};
+		$rootScope.requirementComments = $cookieStore.get('requirementComments') || {};
+
 		$rootScope.globals = $cookieStore.get('globals') || null;
+		$rootScope.project = $cookieStore.get('project') || null;
+		$rootScope.requirement = $cookieStore.get('requirement') || null;
 		$rootScope.$on("$stateChangeStart", changeState);
 
 		function changeState(event, toState, toStateParams) {
 			authService.check().then(function() {
-				if ((toState.name != "login" && !$rootScope.globals) || !authService.check) {
+				if ((toState.name != "login" && !$rootScope.globals)) {
 					event.preventDefault();
 					$state.go("login");
+				} else if((!/project.requirement[^s]/g.test(toState.name) && $rootScope.globals))  {
+					resetRequirement();
 				}
 			}, function() {
 				logout();
@@ -29,6 +38,11 @@
 		function logout() {
 			authService.logout();
 			$state.go('login');
+		};
+
+		function resetRequirement() {
+			$rootScope.requirement = null;
+			$cookieStore.remove('requirement');
 		};
 
 	};
